55e313f0527d102a75995d3c2bbe17659d809c74
nobgkai <okaivit@gmail.com>
2025-06-18 21:26:02 +0700

    update5

--- frontendv2/app/contact/page.js
+++ frontendv2/app/contact/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Head from "next/head";
-export default function Contac() {
+export default function Contact() {
   return (
     <>
       <main className="bg-gray-100 flex items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold text-indigo-600">
-          หน้า ข้อมูล contac
+          หน้า ข้อมูล contact
         </h1>
       </main>
     </>
